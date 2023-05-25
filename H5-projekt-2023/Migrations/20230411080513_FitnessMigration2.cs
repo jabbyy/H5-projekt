cf35ba43e25fe10f82501253c51e24cb726af52a
jabbyy <jeppem65@gmail.com>
2023-05-25 14:41:04 +0200

    Changes

---
+++ H5-projekt-2023/Migrations/20230411080513_FitnessMigration2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace H5_projekt_2023.Migrations
+{
+    /// <inheritdoc />
+    public partial class FitnessMigration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
